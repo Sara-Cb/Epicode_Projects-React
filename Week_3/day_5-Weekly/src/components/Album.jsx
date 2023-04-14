922c1b9265e82ce0e8d2529d8ec6850f8093b732
Sara-Cb <saramaiden23@gmail.com>
2023-04-14 16:58:52 +0200

    manca solo favourites

--- Week_3/day_5-Weekly/src/components/Album.jsx
+++ Week_3/day_5-Weekly/src/components/Album.jsx
@@ -1,7 +1,64 @@
-import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Image } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import TrackCard from "../components/TrackCard";
+
+let headers = new Headers({
+  "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  "X-RapidAPI-Key": "c74a0a086emshf55ffb8dbdcb59ap17a486jsnb83bb4d3e387",
+});
 
 function Album() {
-  return <Container></Container>;
+  const params = useParams();
+  const [album, setAlbum] = useState({});
+
+  useEffect(() => {
+    async function fetchAlbum() {
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/deezer/album/" +
+            params.id,
+          {
+            method: "GET",
+            headers,
+          }
+        );
+        const data = await response.json();
+        setAlbum(data);
+        console.log(album);
+      } catch (error) {
+        // ex.: Url is not correct, Internal Server Error
+        console.log(error);
+      }
+    }
+
+    fetchAlbum();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Container>
+      {album.title && (
+        <div className="mb-4">
+          <Image src={album.cover_medium} fluid />
+          <h1>{album.title}</h1>
+          <h2>
+            <Link to={`/artist/${album.artist.id}`}>{album.artist.name}</Link>
+          </h2>
+        </div>
+      )}
+      {album.tracks &&
+        album.tracks.data.map((track) => (
+          <TrackCard
+            key={track.title}
+            track={track}
+            onClick={() => {
+              // dispatch della track
+            }}
+          />
+        ))}
+    </Container>
+  );
 }
 
 export default Album;
--- Week_3/day_5-Weekly/src/components/Artist.jsx
+++ Week_3/day_5-Weekly/src/components/Artist.jsx
@@ -1,7 +1,74 @@
-import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Image } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import TrackCard from "../components/TrackCard";
+
+let headers = new Headers({
+  "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  "X-RapidAPI-Key": "c74a0a086emshf55ffb8dbdcb59ap17a486jsnb83bb4d3e387",
+});
 
 function Artist() {
-  return <Container></Container>;
+  const params = useParams();
+  const [artist, setArtist] = useState({});
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    async function fetchArtist() {
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/deezer/artist/" +
+            params.id,
+          {
+            method: "GET",
+            headers,
+          }
+        );
+        const data = await response.json();
+        setArtist(data);
+      } catch (error) {
+        // ex.: Url is not correct, Internal Server Error
+        console.log(error);
+      }
+    }
+
+    fetchArtist();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    async function fetchTracks() {
+      try {
+        let response = await fetch(artist.tracklist, {
+          method: "GET",
+          headers,
+        });
+        const data = await response.json();
+        setTracks(data.data);
+        console.log(tracks);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (artist.tracklist) {
+      fetchTracks();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [artist]);
+
+  return (
+    <Container>
+      {artist.name && (
+        <div className="mb-4">
+          <Image src={artist.picture_medium} fluid />
+          <h1>{artist.name}</h1>
+        </div>
+      )}
+      {tracks &&
+        tracks.map((track) => <TrackCard key={track.title} track={track} />)}
+    </Container>
+  );
 }
 
 export default Artist;
