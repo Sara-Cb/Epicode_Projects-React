3f2ab75139a62b3818cd662a74d0352ef55d206c
Sara-Cb <saramaiden23@gmail.com>
2023-04-14 12:22:00 +0200

    wip

--- Week_3/day_5-Weekly/src/components/Home.jsx
+++ Week_3/day_5-Weekly/src/components/Home.jsx
@@ -1,7 +1,23 @@
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import AlbumCard from "./AlbumCard";
 
 function Home() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Row>
+        <h2>Rock</h2>
+        {/* mi servono 4 album rock qui*/}
+      </Row>
+      <Row>
+        <h2>Pop</h2>
+        {/* mi servono 4 album pop qui*/}
+      </Row>
+      <Row>
+        <h2>Hip-hop</h2>
+        {/* mi servono 4 album hip-hop qui*/}
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
